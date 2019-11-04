894701839c1a5ff11d05786f4c99527becf00696
seiry12345 <seiry@mail.ru>
2019-11-05 00:35:15 +0600

    cards

--- src/pages/front/index.js
+++ src/pages/front/index.js
@@ -5,7 +5,9 @@ import '../../assets/scss/main.scss';
 import './front.scss';
 
 // jquery
-
+import 'jquery/dist/jquery.min'
 
 // slick
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick.min'
 
