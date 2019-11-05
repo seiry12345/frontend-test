1c64663376454f93157d454740d9658a18f35985
seiry12345 <seiry@mail.ru>
2019-11-06 01:54:25 +0600

    slick dots

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,61 +1,26 @@
 if ($('.cards').length) {
-  // slick
-  function setBoundries(slick, state) {
-    if (state === 'default') {
-      slick.find('.cards ul.slick-dots li').eq(4).addClass('n-small-1');
-    }
+  function dotsOnInit (_this) {
+    _this.find('.slick-dots .slick-active').addClass('slick-dots--show');
+    _this.find('.slick-dots .slick-active').next().addClass('slick-dots--show');
+    _this.find('.slick-dots .slick-active').next().next().addClass('slick-dots--show');
   }
 
-  var slickSlider = $('.cards .row');
-  var maxDots = 3;
-  var transformXIntervalNext = -18;
-  var transformXIntervalPrev = 18;
+  function dotsOnChange (_this) {
+    _this.find('.slick-dots .slick-active').prev().addClass('slick-dots--show');
+    _this.find('.slick-dots .slick-active').addClass('slick-dots--show');
+    _this.find('.slick-dots .slick-active').next().addClass('slick-dots--show');
+  }
 
-  slickSlider.on('init', function (event, slick) {
-    $(this).find('ul.slick-dots').wrap("<div class='slick-dots-container'></div>");
-    $(this).find('ul.slick-dots li').each(function (index) {
-      $(this).addClass('dot-index-' + index);
-    });
-    $(this).find('ul.slick-dots').css('transform', 'translateX(0)');
-    setBoundries($(this), 'default');
-  });
+  function dotsOnEnd (_this) {
+    _this.find('.slick-dots .slick-active').addClass('slick-dots--show');
+    _this.find('.slick-dots .slick-active').prev().addClass('slick-dots--show');
+    _this.find('.slick-dots .slick-active').prev().prev().addClass('slick-dots--show');
+  }
 
-  var transformCount = 0;
-  slickSlider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-    var totalCount = $(this).find('.slick-dots li').length;
-    if (totalCount > maxDots) {
-      if (nextSlide > currentSlide) {
-        if ($(this).find('ul.slick-dots li.dot-index-' + nextSlide).hasClass('n-small-1')) {
-          if (!$(this).find('ul.slick-dots li:last-child').hasClass('n-small-1')) {
-            transformCount = transformCount + transformXIntervalNext;
-            $(this).find('ul.slick-dots li.dot-index-' + nextSlide).removeClass('n-small-1');
-            var nextSlidePlusOne = nextSlide + 1;
-            $(this).find('ul.slick-dots li.dot-index-' + nextSlidePlusOne).addClass('n-small-1');
-            $(this).find('ul.slick-dots').css('transform', 'translateX(' + transformCount + 'px)');
-            var pPointer = nextSlide - 3;
-            var pPointerMinusOne = pPointer - 1;
-            $(this).find('ul.slick-dots li').eq(pPointerMinusOne).removeClass('p-small-1');
-            $(this).find('ul.slick-dots li').eq(pPointer).addClass('p-small-1');
-          }
-        }
-      } else {
-        if ($(this).find('ul.slick-dots li.dot-index-' + nextSlide).hasClass('p-small-1')) {
-          if (!$(this).find('ul.slick-dots li:first-child').hasClass('p-small-1')) {
-            transformCount = transformCount + transformXIntervalPrev;
-            $(this).find('ul.slick-dots li.dot-index-' + nextSlide).removeClass('p-small-1');
-            var nextSlidePlusOne = nextSlide - 1;
-            $(this).find('ul.slick-dots li.dot-index-' + nextSlidePlusOne).addClass('p-small-1');
-            $(this).find('ul.slick-dots').css('transform', 'translateX(' + transformCount + 'px)');
-            var nPointer = currentSlide + 3;
-            var nPointerMinusOne = nPointer - 1;
-            $(this).find('ul.slick-dots li').eq(nPointer).removeClass('n-small-1');
-            $(this).find('ul.slick-dots li').eq(nPointerMinusOne).addClass('n-small-1');
-          }
-        }
-      }
-    }
-  });
-
+  $('.cards .row').on('init', function () {
+    const _this = $(this);
+    dotsOnInit(_this);
+  })
 
   $('.cards .row').slick({
     slidesToShow: 3,
@@ -65,8 +30,8 @@ if ($('.cards').length) {
     prevArrow: '<button type="button" class="slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"></button>',
     dots: true,
-    infinite: true,
-    speed: 600,
+    infinite: false,
+    speed: 400,
     responsive: [
       {
         breakpoint: 1200,
@@ -80,25 +45,40 @@ if ($('.cards').length) {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          rows: 1
+          rows: 1,
         }
       }
     ]
   });
 
-  // trigger href on card click
-  function targetHref(selector) {
-    const item = $(selector);
-    let href;
+  $('.cards .row').on('afterChange', function (e, slick, currentSlide) {
+    const _this = $(this);
 
-    item.click(function () {
-      if ($(this).find('h3 a').attr('href') !== 'undefined') {
-        href = $(this).find('h3 a').attr('href');
-      }
+    _this.find('.slick-dots li').removeClass('slick-dots--show');
+    dotsOnChange(_this);
 
-      window.location.href = href;
-    });
-  }
+    if (currentSlide === 0) {
+      dotsOnInit(_this);
+    }
 
-  targetHref('.card');
+    if (currentSlide + 1 === slick.slideCount) {
+      dotsOnEnd(_this);
+    }
+  });
 }
+
+// trigger href on card click
+function targetHref(selector) {
+  const item = $(selector);
+  let href;
+
+  item.click(function () {
+    if ($(this).find('h3 a').attr('href') !== 'undefined') {
+      href = $(this).find('h3 a').attr('href');
+    }
+
+    window.location.href = href;
+  });
+}
+
+targetHref('.card');
